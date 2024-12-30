ad99765b49e54ea2ade270fdff0508ed619e6c4a
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-12-30 17:14:33 +0600

    feat: add top button

--- index.js
+++ index.js
@@ -135,3 +135,27 @@ const countdownTimerPlugin = () => {
 };
 
 countdownTimerPlugin();
+
+// КНОПКА НАВЕРХ
+topButtonPlugin = () => {
+	const topButton = document.getElementById("top");
+	topButton.style.visibility = "hidden";
+
+	window.addEventListener("scroll", function () {
+		const scrolled = window.scrollY;
+		if (scrolled > 300) {
+			topButton.style.visibility = "visible";
+		} else {
+			topButton.style.visibility = "hidden";
+		}
+	});
+
+	topButton.addEventListener("click", () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	});
+};
+
+topButtonPlugin();
